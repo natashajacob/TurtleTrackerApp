8176fa51a4c71557db3905fef44811ab93ed1e23
natashajacob <natasha.jacob@duke.edu>
2022-10-04 17:48:27 -0400

    Switch to while loop

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -16,34 +16,39 @@ file_name = 'data/raw/Sara.txt'
 file_object = open(file = file_name, mode = 'r')
 
 #Read contents of file into a list 
-line_list = file_object.readlines()
+lineString = file_object.readline()
+
+
+
+#Extract one data line into a variable
+while lineString:
+
+    #Check to see if the lineString is a data line
+    if lineString[0] in ("#",'u'):
+        lineString = file_object.readline()
+        continue 
+        
+        
+    #Split lineString into a list of items
+    lineData = lineString.split()
+        
+    #Assign variables to items in the lineData list
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData[7]
+        
+    #Print information to the user 
+    print(f'Record {record_id} indicates Sara was seen at {obs_lat}N, {obs_lon}W on {obs_date}')
+    
+    #Move to the next line in the file 
+    lineString = file_object.readline()
+
+
 
 #Close the file object 
 file_object.close()
-
-#Extract one data line into a variable
-for lineString in line_list:
-
-    #Check to see if the lineString is a data line
-    if not lineString[0] in ("#",'u'):
-            continue
-        
-        #Split lineString into a list of items
-        lineData = lineString.split()
-        
-        #Assign variables to items in the lineData list
-        record_id = lineData[0]
-        obs_date = lineData[2]
-        obs_lc = lineData[4]
-        obs_lat = lineData[6]
-        obs_lon = lineData[7]
-        
-        #Print information to the user 
-        print(f'Record {record_id} indicates Sara was seen at {obs_lat}N, {obs_lon}W on {obs_date}')
-
-
-
-
 
 
 
